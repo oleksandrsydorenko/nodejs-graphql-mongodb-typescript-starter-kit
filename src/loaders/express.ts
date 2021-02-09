e3bcc36318a06ff068b6a8b42112351629092148
Oleksandr Sydorenko <oleksandr.sr@gmail.com>
2021-02-09 14:36:39 +0200

    chore: removed compression plugin

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,6 +1,4 @@
-import shrinkRay from 'shrink-ray-current';
 import express, { Application, Request, Response } from 'express';
-
 import routes from '@routes';
 
 interface IResponseError extends Error {
@@ -11,7 +9,6 @@ export default (app: Application): void => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(routes());
-  app.use(shrinkRay());
   // catches 404 error and forwards it
   app.use((_req: Request, _res: Response, next): void => {
     const error: IResponseError = new Error('Not Found');
